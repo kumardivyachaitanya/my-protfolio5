dfd01beb7346c1403d400ef69ee3614f18290cf8
kumardivyachaitanya <124526615+kumardivyachaitanya@users.noreply.github.com>
2024-07-05 09:42:11 +0530

    Update project.js

--- src/data/project.js
+++ src/data/project.js
@@ -1,15 +1,7 @@
-import smartEducationImage from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/smart_education.png';
-import weatherspp from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/weather_icon.png';
-import travelplanner from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/tour_planner.png';
-import courseselector from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/course_selector.png';
-import passwordgenerator from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/password_generator.jpeg';
-import githubdetect from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/githubdevdetective.png';
-import container from '/Users/kumardivyachaitanya/Documents/web devlopment/portfolio-1/src/assets/container.jpeg';
-
 const project = [
     {
         key: 1,
-        img: smartEducationImage,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/smart_education.png",
         name: "Smart Education Classroom",
         description: "StudyNotion aims to provide a seamless and interactive learning experience for students, making education more accessible and engaging. Additionally, the platform serves as a platform for instructors to showcase their expertise and connect with learners across the globe.",
         techStack: ['Node.js', 'Express.js', 'EJS', 'API Documentation', 'React JS','HTML','CSS'],
@@ -19,7 +11,7 @@ const project = [
     },
     {
         key: 2,
-        img: weatherspp,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/weather_icon.png",
         name: "Weather App",
         description: "A weather app grants access to chech the temperature,wind speed and cloud status humidity for a particular location on the world",
         techStack: ['Node.js', 'React JS', 'HTML','CSS','JAVASCRIPT'],
@@ -29,7 +21,7 @@ const project = [
     },
     {
         key: 3,
-        img: travelplanner,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/tour_planner.png",
         name: "Travel Planner",
         description: "The Travel Planner App is a comprehensive tool designed to simplify the process of planning and managing trips. Whether you're a frequent traveler or planning a once-in-a-lifetime adventure",
         techStack: ['HTML','CSS','JAVASCRIPT','React JS'],
@@ -40,7 +32,7 @@ const project = [
     },
     {
         key: 4,
-        img: courseselector,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/course_selector.png",
         name: "Top-Courses-Selector",
         description: "Top Courses Selector is a web application that allows users to browse, filter, and select the best courses available online. The application provides a user-friendly interface to search for courses based on various criteria such as category, rating, and popularity.",
         techStack: ['HTML','CSS','React JS','Javascript'],
@@ -50,7 +42,7 @@ const project = [
     },
     {
         key: 5,
-        img: passwordgenerator,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/password_generator.jpeg",
         name: "Password Creator",
         description: "The Password Creator App is a simple and secure tool designed to help users generate strong, random passwords. This app allows users to create passwords of varying lengths and complexities, ensuring their online accounts remain safe from unauthorized access.",
         techStack: ['HTML','CSS','Javascript','React JS','API Integration'],
@@ -60,7 +52,7 @@ const project = [
     },
     {
         key: 6,
-        img: githubdetect,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/githubdevdetective.png",
         name: "Githhub DevDetection App",
         description: "DevDetective is a web application that allows users to search for GitHub profiles by username. It fetches data from the GitHub API and displays user information such as their avatar, name, bio, repositories, followers, following, and more. The app also includes a dark mode feature for improved user experience.",
         techStack: ['HTML','CSS','Javascript','REACT JS','Api Integration'],
@@ -74,43 +66,39 @@ const otherProjects = [
     {
         key: 1,
         link: "https://counter-app-one-delta.vercel.app/",
-        img: container,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/container.jpeg",
         name: "Counter App"
     },
     {
         key: 8,
         link: "https://github.com/kumardivyachaitanya/Tic-Tac-Toe-game",
-        img: container,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/container.jpeg",
         name:"Tic-Tac-Toe Game"
     },
     {
         key: 9,
         link: "https://github.com/kumardivyachaitanya/Discord-Clone",
-        img: container,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/container.jpeg",
         name:"Discord Clone"
     },
     {
         key: 10,
         link: "https://github.com/kumardivyachaitanya/URL-SHORTNER",
-        img: container,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/container.jpeg",
         name:"Url Shortner"
     },
     {
         key: 11,
         link: "https://github.com/kumardivyachaitanya/razor_pay_01",
-        img: container,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/container.jpeg",
         name:"Razon Pay Clone"
     },
     {
         key: 12,
         link: "https://github.com/kumardivyachaitanya/paralax_effect?tab=readme-ov-file",
-        img: container,
+        img: "https://raw.githubusercontent.com/kumardivyachaitanya/my-protfolio5/main/src/assets/container.jpeg",
         name:"Paralax Effect"
-    },
-
-    
-    
-    
+    }
 ]
 
 const data = { project, otherProjects }
